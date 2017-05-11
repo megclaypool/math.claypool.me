02441fe7c3d977cc97e795088fbd914af4264bb1
Meg Claypool <margaret.claypool@gmail.com>
2017-05-10 18:51:02 -0700

    Let's see if I can successfully update my page through git...

---
+++ test/resources/expand.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	console.log("the script is loading")
+});
+
+
+ $(document).ready(function() {
+
+		$('.concept-name').click(function() {
+
+				if ($(this).parent().is('.open')){
+						$(this).closest('.concept').find('.concept-content-container').animate({'height':'0'},500);
+						$(this).closest('.concept').removeClass('open');
+
+						}else{
+								var newHeight =$(this).closest('.concept').find('.concept-content').height() +'px';
+								$(this).closest('.concept').find('.concept-content-container').animate({'height':newHeight},500);
+								$(this).closest('.concept').addClass('open');
+						}
+
+		});
+
+});
+
+$(document).ready(function() {
+	var elem = $(location.hash);
+	var newHeight =$(elem).closest('.concept').find('.concept-content').height() +'px';
+	$(elem).closest('.concept').find('.concept-content-container').animate({'height':newHeight},500);
+	$(elem).closest('.concept').addClass('open');
+});
